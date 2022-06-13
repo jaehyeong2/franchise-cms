2c42099e06c7682530197de66e29f68a0d70ebd6
jae <wogud1514@naver.com>
2022-06-13 20:30:06 +0900

    시작

---
+++ src/test/java/jjfactory/franchise/cms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.franchise.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
